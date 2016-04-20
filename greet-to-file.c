d8ceb273d0188b1e4f1ffda7650fe79ff795e66f
zarak <zarak.mahmud@utoronto.ca>
2016-04-20 21:22:44 +0000

    Add error checking

--- greet-to-file.c
+++ greet-to-file.c
@@ -1,9 +1,18 @@
 #include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 
 void main()
 {
     int fd;
     fd = open("foo", O_WRONLY | O_CREAT, 0644);
+    if (fd < 0) {
+        printf("error number %d\n", errno);
+        perror("foo");
+        exit(1);
+    }
     write(fd, "hello, world", 11);
     close(fd);
 }
