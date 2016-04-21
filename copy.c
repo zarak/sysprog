f2d9d6558354e3398d5c2e05716e9c770575954d
zarak <zarak.mahmud@utoronto.ca>
2016-04-21 10:43:44 +0000

    Byte copy of a file

---
+++ copy.c
@@ -0,0 +1,27 @@
+/* Trivial file copy program using low-level I/O */
+
+#include <fcntl.h>
+#include <stdlib.h>
+#define BSIZE 16384
+
+void main()
+{
+    int fin, fout; /* Input and output handles */
+    char buf[BSIZE];
+    int count;
+
+    if ((fin = open("foo", O_RDONLY)) < 0) {
+        perror("foo");
+        exit(1);
+    }
+    if ((fout = open("bar", O_WRONLY | O_CREAT, 0644)) < 0) {
+        perror("bar");
+        exit(2);
+    }
+    while ((count = read(fin, buf, BSIZE)) > 0) {
+        write(fout, buf, count);
+    }
+
+    close(fin);
+    close(fout);
+}
